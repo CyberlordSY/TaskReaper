a36302e833473a776507aa0cfbe12ea1c9ae9234
CyberLordSY <cyberlordsy.hacker@gmail.com>
2025-04-17 17:29:54 +0530

    TaskReaper

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,8 @@ const Navbar = () => {
     return (
         <>
             <nav className='flex sticky top-0 justify-between bg-[#4a4a4a] text-white p-2'>
-                <div className="logo">
+                <div className="logo flex items-center">
+                    <img src="/vite.png" className='w-[10vh]' alt="" srcset="" />
                     <span className='font-bold text-xl mx-8'>TaskReaper</span>
                 </div>
                 <ul className='flex gap-8 mx-9'>
